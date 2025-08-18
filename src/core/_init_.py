3b12da305f47672b3eda79a97604e460dea776e0
Martin Robaldo <tincho.robaldo@gmail.com>
2025-08-18 18:32:24 -0300

    Create _init_.py

---
+++ src/core/_init_.py
@@ -0,0 +1,21 @@
+"""
+Core modules for MAGMA Platform
+"""
+
+from .magma_dashboard import *
+from .magma_bot import *
+from .yield_opportunity_scanner import *
+from .market_event_learning_engine import *
+from .live_protocol_simulator import *
+from .community_intelligence_engine import *
+from .educational_context_connector import *
+
+__all__ = [
+    'magma_dashboard',
+    'magma_bot', 
+    'yield_opportunity_scanner',
+    'market_event_learning_engine',
+    'live_protocol_simulator',
+    'community_intelligence_engine',
+    'educational_context_connector'
+]
